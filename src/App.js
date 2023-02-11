e4f80c03d1cb09a8b3af4972fed6bd77ed281085
Joel Adewole <joeladewole3@gmail.com>
2023-02-11 01:00:11 +0100

    Implemented code samples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+// Using the useEffect Hook to Manage Server Requests
+import React, { useEffect, useState } from 'react';
 
-function App() {
+function MyComponent() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>- {item.title}</h2>
+          <p>{item.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default MyComponent
+
+
+
+// Optimizing Server Request Performance with the useMemo Hook
+// ==============================================================
+
+// import { useEffect, useState, useMemo } from 'react';
+
+// function MyComponent({ postId }) {
+//   const [data, setData] = useState({});
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+//       const data = await response.json();
+//       setData(data);
+//     }
+//     fetchData();
+//   }, [postId]);
+
+//   const title = useMemo(() => data.title, [data]);
+
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//     </div>
+//   );
+// }
+
+// export default MyComponent
+
+
+
+
+
+// Managing Server Request State with the useReducer Hook
+// ==============================================================
+
+
+// import { useReducer } from 'react';
+
+// const initialState = { data: [], loading: false, error: '' };
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case 'FETCH_DATA_REQUEST':
+//       return { ...state, loading: true };
+//     case 'FETCH_DATA_SUCCESS':
+//       return { ...state, data: action.payload, loading: false };
+//     case 'FETCH_DATA_FAILURE':
+//       return { ...state, error: action.payload, loading: false };
+//     default:
+//       return state;
+//   }
+// };
+
+// function MyComponent() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+
+//   const fetchData = async () => {
+//     dispatch({ type: 'FETCH_DATA_REQUEST' });
+//     try {
+//       const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//       const data = await response.json();
+//       dispatch({ type: 'FETCH_DATA_SUCCESS', payload: data });
+//     } catch (error) {
+//       dispatch({ type: 'FETCH_DATA_FAILURE', payload: error.message });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       { state.loading && <p>Loading...</p>}
+//       { state.error ? <p>{state.error}</p> 
+//         : <div>
+//           {state.data.map((item) => (
+//             <div key={item.id}>
+//               <h2>{item.title}</h2>
+//               <p>{item.body}</p>
+//             </div>
+//           ))}
+//         </div>
+//       }
+//       <button onClick={fetchData}>Fetch Data</button>
+//     </div>
+//   );
+// }
+
+// export default MyComponent
